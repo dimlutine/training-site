9d8ade44f955e1082dac060d54d07e68cf98403b
Jeremy Rowley <dimlutine@gmail.com>
2022-07-12 07:34:18 -0400

    add routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log('..server');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 8000;
+const sitename = 'Training Site';
+
+const app = express();
+
+app.get('/api', (req, res) => {
+  res.status(200).json({ message: `Welcome to the ${sitename} API` });
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
