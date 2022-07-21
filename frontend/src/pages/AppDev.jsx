a8efed7de9ea9960d6cd8db749ca6d14c355b409
Jeremy Rowley <dimlutine@gmail.com>
2022-07-21 18:21:47 -0400

    add appdev landing

--- frontend/src/pages/AppDev.jsx
+++ frontend/src/pages/AppDev.jsx
@@ -1,4 +1,68 @@
+import { useNavigate, Link } from 'react-router-dom';
+import BackButton from '../components/BackButton';
+
 function AppDev() {
-  return <div>AppDev</div>;
+  const content = [
+    // {
+    //   to: '/',
+    //   name: 'Home',
+    //   key: 1,
+    // },
+    {
+      to: '/java',
+      name: 'Java',
+      key: 2,
+    },
+    {
+      to: '/cplusplus',
+      name: 'C++',
+      key: 3,
+    },
+    {
+      to: '/javascript',
+      name: 'JavaScript',
+      key: 4,
+    },
+    {
+      to: '/python',
+      name: 'Python',
+      key: 5,
+    },
+    {
+      to: '/csharp',
+      name: 'C#',
+      key: 6,
+    },
+    {
+      to: '/rust',
+      name: 'Rust',
+      key: 7,
+    },
+    {
+      to: '/kotlin',
+      name: 'Kotlin',
+      key: 8,
+    },
+  ];
+  return (
+    <>
+      <div>
+        <BackButton url='/' />
+      </div>
+      <section className='heading'>
+        <h1>App Development</h1>
+        <p>Please choose a subject below to get started</p>
+      </section>
+      <main>
+        {content.map((subject) => (
+          <Link to={subject.to}>
+            <button key={subject.key} className='btn btn-block btn-reverse'>
+              {subject.name}
+            </button>
+          </Link>
+        ))}
+      </main>
+    </>
+  );
 }
 export default AppDev;
