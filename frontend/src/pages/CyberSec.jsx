ccef65a07231e94bf49983e38fb30367a149f32e
Jeremy Rowley <dimlutine@gmail.com>
2022-07-22 07:31:18 -0400

    add cybersec landing

--- frontend/src/pages/CyberSec.jsx
+++ frontend/src/pages/CyberSec.jsx
@@ -1,4 +1,68 @@
+import { useNavigate, Link } from 'react-router-dom';
+import BackButton from '../components/BackButton';
+
 function CyberSec() {
-  return <div>CyberSec</div>;
+  const content = [
+    // {
+    //   to: '/',
+    //   name: 'Home',
+    //   key: 1,
+    // },
+    {
+      to: '/appsec',
+      name: 'Application Security',
+      key: 2,
+    },
+    {
+      to: '/dlp',
+      name: 'Data Loss Prevention',
+      key: 3,
+    },
+    {
+      to: '/forensics',
+      name: 'Forensics',
+      key: 4,
+    },
+    {
+      to: '/incidentresponse',
+      name: 'Incident Response',
+      key: 5,
+    },
+    {
+      to: '/netsec',
+      name: 'Network Security',
+      key: 6,
+    },
+    {
+      to: '/threatintel',
+      name: 'Threat Intelligence',
+      key: 7,
+    },
+    {
+      to: '/vulnmgmt',
+      name: 'Vulnerability Management',
+      key: 8,
+    },
+  ];
+  return (
+    <>
+      <div>
+        <BackButton url='/' />
+      </div>
+      <section className='heading'>
+        <h1>Cyber Security</h1>
+        <p>Please choose a subject below to get started</p>
+      </section>
+      <main>
+        {content.map((subject) => (
+          <Link to={subject.to}>
+            <button key={subject.key} className='btn btn-block btn-reverse'>
+              {subject.name}
+            </button>
+          </Link>
+        ))}
+      </main>
+    </>
+  );
 }
 export default CyberSec;
