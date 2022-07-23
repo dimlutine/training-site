af55e8377abd4eaa51276f75100aa55df7f39250
Jeremy Rowley <dimlutine@gmail.com>
2022-07-23 12:14:37 -0400

    add devops landing

--- frontend/src/pages/DevOps.jsx
+++ frontend/src/pages/DevOps.jsx
@@ -1,4 +1,73 @@
+import { useNavigate, Link } from 'react-router-dom';
+import BackButton from '../components/BackButton';
+
 function DevOps() {
-  return <div>DevOps</div>;
+  const content = [
+    // {
+    //   to: '/',
+    //   name: 'Home',
+    //   key: 1,
+    // },
+    {
+      to: '/linux',
+      name: 'Linux',
+      key: 2,
+    },
+    {
+      to: '/buildtools',
+      name: 'Build Tools',
+      key: 3,
+    },
+    {
+      to: '/continuousintdep',
+      name: 'Continuous Integration and Deployment',
+      key: 4,
+    },
+    {
+      to: '/scripting',
+      name: 'Scripting',
+      key: 5,
+    },
+    {
+      to: '/cloudtech',
+      name: 'Cloud Technology',
+      key: 6,
+    },
+    {
+      to: '/monitoringtools',
+      name: 'Monitoring Tools',
+      key: 7,
+    },
+    {
+      to: '/configmgmt',
+      name: 'Configuration Management',
+      key: 8,
+    },
+    {
+      to: '/contvirt',
+      name: 'Containerization and Virtualization',
+      key: 9,
+    },
+  ];
+  return (
+    <>
+      <div>
+        <BackButton url='/' />
+      </div>
+      <section className='heading'>
+        <h1>DevOps</h1>
+        <p>Please choose a subject below to get started</p>
+      </section>
+      <main>
+        {content.map((subject) => (
+          <Link to={subject.to}>
+            <button key={subject.key} className='btn btn-block btn-reverse'>
+              {subject.name}
+            </button>
+          </Link>
+        ))}
+      </main>
+    </>
+  );
 }
 export default DevOps;
