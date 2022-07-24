2237302e9312a711fc4d09e28616a3d12cc3ee82
Jeremy Rowley <dimlutine@gmail.com>
2022-07-24 18:08:40 -0400

    add hardware landing

--- frontend/src/pages/Hardware.jsx
+++ frontend/src/pages/Hardware.jsx
@@ -1,4 +1,63 @@
+import { useNavigate, Link } from 'react-router-dom';
+import BackButton from '../components/BackButton';
+
 function Hardware() {
-  return <div>Hardware</div>;
+  const content = [
+    // {
+    //   to: '/',
+    //   name: 'Home',
+    //   key: 1,
+    // },
+    {
+      to: '/overview',
+      name: 'Overview',
+      key: 2,
+    },
+    {
+      to: '/desktop',
+      name: 'Desktop Hardware',
+      key: 3,
+    },
+    {
+      to: '/laptop',
+      name: 'Laptop Hardware',
+      key: 4,
+    },
+    {
+      to: '/server',
+      name: 'Server Hardware',
+      key: 5,
+    },
+    {
+      to: '/mobile',
+      name: 'Mobile Devices',
+      key: 6,
+    },
+    {
+      to: '/iot',
+      name: 'IoT Devices',
+      key: 7,
+    },
+  ];
+  return (
+    <>
+      <div>
+        <BackButton url='/' />
+      </div>
+      <section className='heading'>
+        <h1>Hardware</h1>
+        <p>Please choose a subject below to get started</p>
+      </section>
+      <main>
+        {content.map((subject) => (
+          <Link to={subject.to}>
+            <button key={subject.key} className='btn btn-block btn-reverse'>
+              {subject.name}
+            </button>
+          </Link>
+        ))}
+      </main>
+    </>
+  );
 }
 export default Hardware;
