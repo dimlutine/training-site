59f818a7d106d22649183f55f1f28489243baf8c
Jeremy Rowley <dimlutine@gmail.com>
2022-07-25 07:18:11 -0400

    add software landing

--- frontend/src/pages/Software.jsx
+++ frontend/src/pages/Software.jsx
@@ -1,4 +1,53 @@
+import { useNavigate, Link } from 'react-router-dom';
+import BackButton from '../components/BackButton';
+
 function Software() {
-  return <div>Software</div>;
+  const content = [
+    // {
+    //   to: '/',
+    //   name: 'Home',
+    //   key: 1,
+    // },
+    {
+      to: '/overview',
+      name: 'Overview',
+      key: 2,
+    },
+    {
+      to: '/win10',
+      name: 'Windows 10',
+      key: 3,
+    },
+    {
+      to: '/win11',
+      name: 'Windows 11',
+      key: 4,
+    },
+    {
+      to: '/msoffice',
+      name: 'Microsoft Office',
+      key: 5,
+    },
+  ];
+  return (
+    <>
+      <div>
+        <BackButton url='/' />
+      </div>
+      <section className='heading'>
+        <h1>Software</h1>
+        <p>Please choose a subject below to get started</p>
+      </section>
+      <main>
+        {content.map((subject) => (
+          <Link to={subject.to}>
+            <button key={subject.key} className='btn btn-block btn-reverse'>
+              {subject.name}
+            </button>
+          </Link>
+        ))}
+      </main>
+    </>
+  );
 }
 export default Software;
